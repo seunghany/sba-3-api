7863e7a84a958bc7c22644aa5bd41de44ca273c0
Seunghan <seunghany2@gmail.com>
2020-10-06 09:38:09 +0900

    update

--- model/__init__.py
+++ model/__init__.py
@@ -5,5 +5,8 @@ from model.iris_model import IrisModel
 
 if __name__ == '__main__':
     iris = IrisModel()
-
-    
+    # iris.draw_scatter()
+    # iris.draw_errors()
+    # iris.plot_decision_regions()
+    iris.draw_adaline_graph()
+    # iris.draw_adaline_gd_graph()
--- model/perceptron.py
+++ model/perceptron.py
@@ -4,7 +4,6 @@ import numpy as np
 
 class Perceptron(object):
     """퍼셉트론 분류기
-
     매개변수
     ------------
     eta : float
@@ -13,14 +12,12 @@ class Perceptron(object):
       훈련 데이터셋 반복 횟수
     random_state : int
       가중치 무작위 초기화를 위한 난수 생성기 시드
-
     속성
     -----------
     w_ : 1d-array
       학습된 가중치
     errors_ : list
       에포크마다 누적된 분류 오류
-
     """
     def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
@@ -29,18 +26,15 @@ class Perceptron(object):
 
     def fit(self, X, y):
         """훈련 데이터 학습
-
         매개변수
         ----------
         X : {array-like}, shape = [n_samples, n_features]
           n_samples개의 샘플과 n_features개의 특성으로 이루어진 훈련 데이터
         y : array-like, shape = [n_samples]
           타깃값
-
         반환값
         -------
         self : object
-
         """
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
